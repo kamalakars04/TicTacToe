695c8e90636b829fbb7e8de5f7aadf847ea2705e
kamalakars04 <kamalakars04@gmail.com>
2020-10-08 20:48:12 +0530

    [kamalakar].[refactor].Added UC7_PositionOfGame and Fixed Bugs in Other Methods.

--- Program.cs
+++ Program.cs
@@ -7,17 +7,26 @@ namespace TicTacToe
         
         static void Main(string[] args)
         {
-            int numOfTurn = 1;
             Console.WriteLine("Welcome To TicTacToe");
-            TicTacToe ticTacToe = new TicTacToe();
-            //To create a board
-            ticTacToe.CreateBoard();
-            // If user doesnt select correct input
-            if (!ticTacToe.LetterSelection())
-                return;
-            ticTacToe.ShowBoard();
-            int playerNum = ticTacToe.Toss();
-            if (!ticTacToe.SelectIndex(playerNum)) return;
+            while(true)
+            {
+                TicTacToe ticTacToe = new TicTacToe();
+                //To create a board
+                ticTacToe.CreateBoard();
+                //show the board
+                ticTacToe.ShowBoard();
+                string firstTurn = ticTacToe.Toss();
+                // If user doesnt select correct input and want to exit
+                if (!ticTacToe.LetterSelection(firstTurn))
+                    return;
+                //If user want to exit in between the game
+                if (!ticTacToe.MakeMove(firstTurn)) 
+                    return;
+                Console.WriteLine("Enter E to exit or any other key to Play Again");
+                //If user doesnt want to play again
+                if (Console.ReadLine().ToUpper() != "E")
+                    return;
+            }
         }
     }
 }
